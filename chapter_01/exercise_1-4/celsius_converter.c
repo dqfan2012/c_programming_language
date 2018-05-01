49f46e37a2a7885886bdb4e0f26251d24b779057
Samuel Stidham <dqfan2012@gmail.com>
2018-04-30 19:29:02 -0700

    Added spaces at the end of exercises 1-1 and 1-2. Completed Exercises 1-3 and 1-4.

---
+++ chapter_01/exercise_1-4/celsius_converter.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/*print Celsius-Fahrenheit Table
+    for celsius = 0, 20, ..., 300 */
+
+int main()
+{
+    float fahr, celsius;
+    int lower, upper, step;
+
+    lower = 0.0; /* lower limit of temperature table */
+    upper = 300; /* upper limit */
+    step = 20; /* step size */
+
+    printf("%s %s\n", "Celsius", "Farenheit");
+
+    celsius = lower;
+    while (celsius <= upper) {
+        fahr = (9.0/5.0 * celsius) + 32.0;
+        printf("%6.1f %7.1f\n", celsius, fahr);
+        celsius = celsius + step;
+    }
+}
