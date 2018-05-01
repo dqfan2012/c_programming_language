b60a148f840f8a761757564384e503186e6f9413
Samuel Stidham <dqfan2012@gmail.com>
2018-04-30 18:49:36 -0700

    Finished following the instructions for Exercise 1-1

--- chapter_01/exercise_1-1/hello.c
+++ chapter_01/exercise_1-1/hello.c
@@ -1,6 +1,39 @@
 #include <stdio.h>
 
+// Exercise 1-1 Run the "hello, world" program on your system.
+//  Experiment with leaving out parts of the program, to see what error messages you get.
+
 int main()
 {
+    // Regular "hello, world" program
     printf("hello, world\n");
+
+    /*
+     * "Forgetting" the semi-colon
+     *
+     * Error:
+     *
+     * hello.c: In function ‘main’:
+     * hello.c:13:1: error: expected ‘;’ before ‘}’ token
+     *  }
+     *  ^
+     */
+    //printf("hello, world\n")
+
+    /*
+     *
+     * hello.c: In function ‘main’:
+     * hello.c:24:12: warning: missing terminating " character
+     *      printf("hello, world\n);
+     *             ^
+     * hello.c:24:12: error: missing terminating " character
+     * printf("hello, world\n);
+     *        ^~~~~~~~~~~~~~~~~
+     *
+     * hello.c:25:1: error: expected expression before ‘}’ token
+     * }
+     * ^
+     * hello.c:25:1: error: expected ‘;’ before ‘}’ token
+     */
+    //printf("hello, world\n);
 }
